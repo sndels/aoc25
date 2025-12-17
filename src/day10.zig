9c9824e8c961f561d2b342ed17dd1d14b481ac98
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2025-12-17 09:24:37 +0200

    Start on day 10 part 2
    Need do this smarter...

--- src/day10.zig
+++ src/day10.zig
@@ -10,8 +10,8 @@ const Button = struct {
     end: usize,
 };
 
-const LightState = struct {
-    lights: [10]u8, // Input only has <10 lights for any machine
+const MachineState = struct {
+    indicators: [10]i32, // Input only has <10 lights/joltages for any machineh
     presses: usize,
     previous_press: usize,
 };
@@ -34,19 +34,19 @@ pub fn main() !void {
 
     // Day code here
     var line_it = std.mem.splitScalar(u8, input, '\n');
-    var target_lights = try std.array_list.Managed(u8).initCapacity(gpa, 10);
-    defer target_lights.deinit();
+    var target_indicators = try std.array_list.Managed(i32).initCapacity(gpa, 10);
+    defer target_indicators.deinit();
     var buttons_backing = std.array_list.Managed(u8).init(gpa);
     defer buttons_backing.deinit();
     var buttons = std.array_list.Managed(Button).init(gpa);
     defer buttons.deinit();
-    var states = std.array_list.Managed(LightState).init(gpa);
-    defer states.deinit();
-    var seen_lights = std.AutoHashMap([10]u8, void).init(gpa);
-    defer seen_lights.deinit();
+    var machine_states = std.array_list.Managed(MachineState).init(gpa);
+    defer machine_states.deinit();
+    var seen_states = std.AutoHashMap([10]i32, void).init(gpa);
+    defer seen_states.deinit();
     var press_sum: usize = 0;
     while (line_it.next()) |line| {
-        target_lights.clearRetainingCapacity();
+        target_indicators.clearRetainingCapacity();
         buttons_backing.clearRetainingCapacity();
         buttons.clearRetainingCapacity();
         try buttons.append(Button{ .begin = 0, .end = 0 });
@@ -60,7 +60,16 @@ pub fn main() !void {
             switch (parse_state) {
                 ParseState.Lights => {
                     switch (c) {
-                        '#', '.' => target_lights.appendAssumeCapacity(c),
+                        '#' => {
+                            if (part == 1) {
+                                target_indicators.appendAssumeCapacity(1);
+                            }
+                        },
+                        '.' => {
+                            if (part == 1) {
+                                target_indicators.appendAssumeCapacity(0);
+                            }
+                        },
                         ']' => {
                             parse_state = ParseState.Button;
                             i += 1;
@@ -95,28 +104,45 @@ pub fn main() !void {
                         else => unreachable,
                     }
                 },
-                ParseState.Joltages => break :line_loop,
+                ParseState.Joltages => {
+                    if (part == 2) {
+                        const end = if (std.mem.indexOfScalar(u8, line[i..], '}')) |ei| ei else 0;
+                        assert(end > 0);
+                        var joltage_it = std.mem.splitScalar(u8, line[i..(i + end)], ',');
+                        while (joltage_it.next()) |joltage| {
+                            target_indicators.appendAssumeCapacity(try std.fmt.parseInt(i32, joltage, 10));
+                        }
+                    }
+                    break :line_loop;
+                },
             }
         }
-        // dbgPrint("{s} {any} {any}\n", .{ target_lights.items, buttons_backing.items, buttons.items });
+        dbgPrint("{any} {any} {any}\n", .{ target_indicators.items, buttons_backing.items, buttons.items });
 
-        seen_lights.clearRetainingCapacity();
-        states.clearRetainingCapacity();
-        try states.append(LightState{
-            .lights = [_]u8{'.'} ** 10,
+        seen_states.clearRetainingCapacity();
+        machine_states.clearRetainingCapacity();
+        try machine_states.append(MachineState{
+            .indicators = [_]i32{0} ** 10,
             .presses = 0,
             .previous_press = 10,
         });
-        try seen_lights.put(states.items[0].lights, {});
-        while (true) {
-            const source_state = states.orderedRemove(0);
-            if (std.mem.eql(u8, target_lights.items, source_state.lights[0..target_lights.items.len])) {
+        try seen_states.put(machine_states.items[0].indicators, {});
+        state_loop: while (true) {
+            const source_state = machine_states.orderedRemove(0);
+            if (std.mem.eql(i32, target_indicators.items, source_state.indicators[0..target_indicators.items.len])) {
                 dbgPrint("{d} needed\n", .{source_state.presses});
                 press_sum += source_state.presses;
                 break;
             }
 
-            for (buttons.items, 0..) |button, bi| {
+            if (part == 2) {
+                for (0..target_indicators.items.len) |ti| {
+                    if (target_indicators.items[ti] < source_state.indicators[ti])
+                        continue :state_loop;
+                }
+            }
+
+            button_loop: for (buttons.items, 0..) |button, bi| {
                 if (bi == source_state.previous_press)
                     continue;
                 var state = source_state;
@@ -124,17 +150,25 @@ pub fn main() !void {
                 state.presses += 1;
                 for (button.begin..button.end) |lbi| {
                     const li = buttons_backing.items[lbi];
-                    const light = &state.lights[li];
-                    if (light.* == '#') {
-                        light.* = '.';
+                    const indicator = &state.indicators[li];
+                    if (indicator.* == target_indicators.items[li])
+                        continue :button_loop;
+                    if (part == 1) {
+                        if (indicator.* == 0) {
+                            indicator.* = 1;
+                        } else {
+                            indicator.* = 0;
+                        }
                     } else {
-                        light.* = '#';
+                        // TODO:
+                        // Really need to do multiple presses at once or something when the joltages are in the hundreds.
+                        indicator.* += 1;
                     }
                 }
-                if (seen_lights.contains(state.lights))
+                if (seen_states.contains(state.indicators))
                     continue;
-                try states.append(state);
-                try seen_lights.put(state.lights, {});
+                try machine_states.append(state);
+                try seen_states.put(state.indicators, {});
             }
         }
     }
